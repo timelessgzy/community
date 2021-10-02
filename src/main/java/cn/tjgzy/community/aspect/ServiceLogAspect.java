6b805ca001520d9532eb3a96bc069bf9c11d162c
Gzy <854606081@qq.com>
2021-10-02 20:49:36 +0800

    完成了统一异常处理与日志记录

---
+++ src/main/java/cn/tjgzy/community/aspect/ServiceLogAspect.java
@@ -0,0 +1,46 @@
+package cn.tjgzy.community.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author GongZheyi
+ * @create 2021-10-02-20:38
+ */
+@Component
+@Aspect
+public class ServiceLogAspect {
+    private static final Logger logger = LoggerFactory.getLogger(ServiceLogAspect.class);
+
+    /**
+     * 声明切点
+     */
+    @Pointcut("execution(* cn.tjgzy.community.service.*.*(..))")
+    public void pointcut() {
+
+    }
+
+    @Before("pointcut()")
+    public void before(JoinPoint joinPoint) {
+        // 用户[ip] 在 xxx 访问了 XXX方法
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        String ip = request.getRemoteHost();
+        String now = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        String target = joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName();
+        logger.info(String.format("用户[%s],在[%s],访问了[%s].",ip, now, target));
+    }
+
+}
--- src/main/java/cn/tjgzy/community/controller/HomeController.java
+++ src/main/java/cn/tjgzy/community/controller/HomeController.java
@@ -47,4 +47,9 @@ public class HomeController {
         model.addAttribute("discussPosts",discussPosts);
         return "/index";
     }
+
+    @GetMapping("/error")
+    public String getErrorPage() {
+        return "/error/500";
+    }
 }
